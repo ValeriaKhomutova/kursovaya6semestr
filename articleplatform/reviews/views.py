8ecf7132427678c629279268f39b0dbc1fc507db
ValeriaKhomutova <vkhomutova2005@gmail.com>
2026-05-17 11:05:15 +0300

    пофикшена логика лайков

--- articleplatform/reviews/views.py
+++ articleplatform/reviews/views.py
@@ -7,20 +7,86 @@ from .models import Review
 
 
 class ReviewCreate(APIView):
-    permission_classes = [permissions.IsAuthenticated]
+
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
 
     def post(self, request):
-        serializer = ReviewCreateSerializer(data=request.data)
 
-        if serializer.is_valid():
-            serializer.save(author=request.user)
+        article_id = request.data.get("article")
+
+        existing_review = Review.objects.filter(
+            author=request.user,
+            article_id=article_id
+        ).first()
+
+        # ЕСЛИ review уже существует
+        if existing_review:
+
+            like = request.data.get(
+                "like",
+                existing_review.like
+            )
+
+            dislike = request.data.get(
+                "dislike",
+                existing_review.dislike
+            )
+
+            # toggle logic
+
+            if existing_review.like and like:
+                like = False
+
+            elif existing_review.dislike and dislike:
+                dislike = False
+
+            elif like:
+                dislike = False
+
+            elif dislike:
+                like = False
+
+            existing_review.text = request.data.get(
+                "text",
+                existing_review.text
+            )
+
+            existing_review.like = like
+            existing_review.dislike = dislike
+
+            existing_review.save()
 
             return Response(
-                ReviewCreateSerializer(serializer.instance).data,
+                ReviewCreateSerializer(
+                    existing_review
+                ).data,
+                status=status.HTTP_200_OK
+            )
+
+        # СОЗДАНИЕ НОВОГО
+
+        serializer = ReviewCreateSerializer(
+            data=request.data
+        )
+
+        if serializer.is_valid():
+
+            serializer.save(
+                author=request.user
+            )
+
+            return Response(
+                serializer.data,
                 status=status.HTTP_201_CREATED
             )
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
     
 class ReviewDelete(APIView):
     permission_classes = [permissions.IsAuthenticated]
