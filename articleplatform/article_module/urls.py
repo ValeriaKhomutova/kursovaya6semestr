c8b9f3356bbbb000fdb0b1d16d18373e9ce9bcb3
ValeriaKhomutova <vkhomutova2005@gmail.com>
2026-06-20 23:55:25 +0300

    hotfix name for test

--- articleplatform/article_module/urls.py
+++ articleplatform/article_module/urls.py
@@ -9,7 +9,7 @@ from .views import (
 
 
 urlpatterns = [
-    path('article/create/', ArticleCreate.as_view(), name='create'),
+    path('article/create/', ArticleCreate.as_view(), name='article-create'),
     path('article/list/', ArticleList.as_view(), name='list'),
     path('article/mylist/', MyArticleList.as_view(), name='mylist'),
     path('article/<int:pk>/update/', ArticleUpdate.as_view(), name='update'),
